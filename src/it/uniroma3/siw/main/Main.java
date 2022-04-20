cf2edb631769351a78acf23358ea0ed5796430c9
Filippo Visconti <filippo.visconti23@gmail.com>
2022-04-20 19:20:07 +0200

    added main and persistence.xml files

--- src/it/uniroma3/siw/main/Main.java
+++ src/it/uniroma3/siw/main/Main.java
@@ -1,9 +1,18 @@
 package it.uniroma3.siw.main;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("courses-unit-test");
+		EntityManager em = emf.createEntityManager();
+
+		em.close();
+		emf.close();
 
 	}
 
